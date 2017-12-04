bd6bada2c109bf8474135a0dcfe2e8ff2c3fe8e5
Brittany Morris <brittmmendez@gmail.com>
2017-12-04 14:58:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+def prime?(num)
+     if num<=1
+       puts "false"
+     else if num.even?
+       puts false
+
+     elsif num>1
+       counter = (num-1)
+       until counter == 1
+         if num % counter==0
+           puts "false"
+          end
+           counter=counter- 1
+       end
+      else
+        puts "true"
+      end
+    end
+end
+  
